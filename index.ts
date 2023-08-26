e3384ad2bc4c65e5d36efa1c80800548fb1528df
Kamil <kamgre7@gmail.com>
2023-08-26 17:08:45 +0200

    Added error handler

--- index.ts
+++ index.ts
@@ -2,12 +2,15 @@ import express from 'express';
 import 'express-async-errors';
 import { appConfig } from './src/config/appConfig';
 import { gitToolRouter } from './src/routes/gitToolRouter';
+import { errorHandler } from './src/middlewares/errorHandler';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/gittool', gitToolRouter);
+
+app.use(errorHandler);
 
 app.listen(appConfig.port, appConfig.hostName, () => {
   console.log(`Application is running on ${appConfig.host}:${appConfig.port}`);
