e7b815ae79c55fb854f7592678440458d9fbfd90
Kamil <kamgre7@gmail.com>
2023-08-28 19:21:24 +0200

    Added service for zipFIle

--- src/domains/gitTools/services/gitToolsService.ts
+++ src/domains/gitTools/services/gitToolsService.ts
@@ -1,3 +1,8 @@
+import {
+  IZipHandler,
+  UnzipFileInfo,
+  zipHandler,
+} from '../../zipHandler/zipHandler';
 import { IGithubApiAdapter, githubApi } from '../adapters/githubApiAdapter';
 import { FileInformation, RepoInformation } from '../schemas/postCommitSchema';
 
@@ -20,11 +25,17 @@ export interface IGitToolsService {
     repoInfo: RepoInformation,
     file: FileInformation
   ): Promise<string>;
-  sendMultipleFiles(): Promise<void>;
+  createCommitFromZip(
+    repoInfo: RepoInformation,
+    zipFile: FileInformation
+  ): Promise<string>;
 }
 
 export class GitToolsService implements IGitToolsService {
-  constructor(private readonly githubApi: IGithubApiAdapter) {}
+  constructor(
+    private readonly githubApi: IGithubApiAdapter,
+    private readonly zipHandler: IZipHandler
+  ) {}
 
   async createCommit(
     repoInfo: RepoInformation,
@@ -60,7 +71,50 @@ export class GitToolsService implements IGitToolsService {
     return await this.githubApi.updateRefs(owner, repo, branch, commitSha);
   }
 
-  async sendMultipleFiles(): Promise<void> {}
+  async createCommitFromZip(
+    repoInfo: RepoInformation,
+    zipFile: FileInformation
+  ): Promise<string> {
+    const { branch, commitMsg, githubToken, owner, repo, path } = repoInfo;
+    const filesInfo = this.zipHandler.getFilesInformation(zipFile.buffer);
+
+    const blobsSha = await this.getMultipleBlobsSha(owner, repo, filesInfo);
+    const baseTreeSha = await this.githubApi.getBaseTree(owner, repo, branch);
+
+    const blobs = this.createBlobTreeObject(blobsSha, path);
+
+    const treeSha = await this.githubApi.createTree(
+      owner,
+      repo,
+      baseTreeSha,
+      blobs
+    );
+
+    const refsSha = await this.githubApi.getRefs(owner, repo, branch);
+    const commitSha = await this.githubApi.createCommit(
+      owner,
+      repo,
+      commitMsg,
+      treeSha,
+      refsSha
+    );
+
+    return await this.githubApi.updateRefs(owner, repo, branch, commitSha);
+  }
+
+  private async getMultipleBlobsSha(
+    owner: string,
+    repo: string,
+    filesInfo: UnzipFileInfo[]
+  ): Promise<BlobShaWithFilename[]> {
+    return await Promise.all(
+      filesInfo.map(async (file) => {
+        const sha = await this.githubApi.createBlob(owner, repo, file.buffer);
+
+        return { sha, filename: file.originalName };
+      })
+    );
+  }
 
   private createBlobTreeObject(
     dataArray: BlobShaWithFilename[],
@@ -75,4 +129,4 @@ export class GitToolsService implements IGitToolsService {
   }
 }
 
-export const gitToolsService = new GitToolsService(githubApi);
+export const gitToolsService = new GitToolsService(githubApi, zipHandler);
