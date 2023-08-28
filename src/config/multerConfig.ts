b5c04122ad48c21e4c243645ecd4c52b11a1c6d8
Kamil <kamgre7@gmail.com>
2023-08-28 19:18:58 +0200

    Added multer config for zip files

--- src/config/multerConfig.ts
+++ src/config/multerConfig.ts
@@ -1,4 +1,20 @@
-import multer from 'multer';
+import { Request } from 'express';
+import multer, { FileFilterCallback } from 'multer';
+import { BadRequestError } from '../errors/BadRequestError';
 
 const storage = multer.memoryStorage();
 export const upload = multer({ storage });
+
+const zipFileFilter = (
+  req: Request,
+  file: Express.Multer.File,
+  cb: FileFilterCallback
+) => {
+  if (file.mimetype === 'application/zip') {
+    cb(null, true);
+  } else {
+    cb(new BadRequestError('Only ZIP files are allowed.'));
+  }
+};
+
+export const zipUpload = multer({ storage, fileFilter: zipFileFilter });
